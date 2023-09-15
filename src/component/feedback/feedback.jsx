13d5910ba8a6375b9e0e3b7ccc57a4c850efe13d
Nazar Melnik <nazar97melnik@gmail.com>
2023-09-16 00:42:15 +0300

    add autoplay for slider

--- src/component/feedback/feedback.jsx
+++ src/component/feedback/feedback.jsx
@@ -20,6 +20,8 @@ export default function Feedback() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
   };
   return (
     <section className="feedback-section" id="feedback">
