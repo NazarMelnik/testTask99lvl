8d9b0ac3ff17042879fd68b0a8e44f83cacef18e
Nazar Melnik <nazar97melnik@gmail.com>
2023-09-18 18:20:42 +0300

    fix mistake

--- src/component/form/form.jsx
+++ src/component/form/form.jsx
@@ -34,7 +34,7 @@ export default function Form() {
         initial={{ opacity: 0, x: 200 }}
         whileInView={{
           opacity: 1,
-          y: 0,
+          x: 0,
           transition: { duration: 1, ease: "easeOut" },
         }}>
         <TextField
